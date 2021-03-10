f18fcb61dacf8b202f84f71ae702b987d1fbf9d9
niceshot <wanqiufeng@outlook.com>
2021-03-10 13:57:33 +0800

    初始化。

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 "# spring-batch-learn" 
+
+参考资料：
+    https://www.baeldung.com/spring-boot-spring-batch
+    https://docs.spring.io/spring-batch/docs/4.3.x/reference/html/
---
+++ src/main/java/com/vincent/springbatch/SpringBatchTest.java
@@ -0,0 +1,12 @@
+package com.vincent.springbatch;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTest {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchTest.class, args);
+    }
+}
